801ac6a541fe9712698edd18d645481be9de0c1d
Tim Klein <timjklein36@gmail.com>
2018-03-30 16:59:07 -0400

    Only load from env vars

--- util.py
+++ util.py
@@ -26,29 +26,15 @@ def random_location_in_circle(center, radius):
     return Location(lat=p[0], lng=p[1])
 
 def load_credentials():
-    cred_filename = "token.txt"
     client_id = os.environ.get('LYFT_CLIENT_ID')
     client_secret = os.environ.get('LYFT_CLIENT_SECRET')
 
     if client_id == None or client_secret == None:
-        creds_found = [False, False]
-        try:
-            with open(cred_filename) as lyft_creds_file:
-                for line in lyft_creds_file:
-                    if line.split("=")[0] == 'LYFT_CLIENT_ID':
-                        client_id = line.split("=")[1]
-                        creds_found[0] = True
-                    elif line.split("=")[0] == 'LYFT_CLIENT_SECRET':
-                        client_secret = line.split("=")[1]
-                        creds_found[1] = True
-        except IOError as error:
-            print("\nError Loading Lyft Credentials!")
-            print("Set as Environment Variables or", \
-                    f"declare in '{cred_filename}'\n")
-            sys.exit(1)
-
-        if not all(cred == True for cred in creds_found):
-            print("\nError: Lyft Credentials", \
-                    f"in '{cred_filename}' are invalid\n")
-            sys.exit(1)
-    return client_id, client_secret
+        error_message = "\nError loading Lyft Credentials.\n" + \
+            "Set credentials as environment variables:\n" + \
+            "   LYFT_CLIENT_ID\n" + "   LYFT_CLIENT_SECRET\n" + \
+            "or consider adding to your shell profile script.\n"
+        print(error_message)
+        sys.exit(1)
+    else:
+        return client_id, client_secret
